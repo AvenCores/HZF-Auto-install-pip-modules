69171c536627ad69054ad01c7b397aea534b028b
AvenCores <avencores@gmail.com>
2022-11-15 18:40:57 +0300

    first commit

---
+++ Installer.py
@@ -0,0 +1,13 @@
+from os import system,name
+
+system('cls' if name == 'nt' else 'clear')
+print("Установка библиотек")
+system("pip3 install --upgrade pip")
+bib = ["MODULE", "MODULE"]
+for i in range(len(bib)):
+    print("\n"+"Установка "+bib[i]+"...\033[0m")
+    system("pip3 install "+bib[i])
+system('cls' if name == 'nt' else 'clear')
+print("Установка завершена!")
+print("\nНажмите Enter чтобы закрыть")
+input()
